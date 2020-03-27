5842127461bafa55516383f58020c5c0aadac2f2
Matheus Porto <ccmatheusporto@hotmail.com>
2020-03-27 14:14:40 -0300

    Adicionado registro de novos usuários com encriptação das senhas

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
-  resources :contacts
-  
   root to: "static_pages#index"
 
   get 'sobre', to: 'static_pages#sobre'
   get 'contato', to: 'static_pages#contato'
+
+    resources :contacts
+    resources :users, only: [:new, :create]
 
 end
